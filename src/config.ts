efb6625dd85f8be3af64dbc2bd11e57037f02b51
Fadli99 <44110432+Fadli99@users.noreply.github.com>
2024-07-04 14:54:22 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE = {
     // Your site's detail?
     name: 'Me',
-    title: 'FAS',
+    title: 'Astro - Me',
     description: 'Crisp, minimal, personal portfolio theme for Astro',
     url: 'https://astro-me.vercel.app',
     githubUrl: 'https://github.com/one-aalam/astro-me'
